60717073dff175c9eb8d2a9e7c9f3a5b9e8c9094
mathieu.ancelin <mathieu.ancelin@2312e98e-8c1d-11de-b664-3b115b7b7a9b>
2010-07-21 19:20:09 +0000

    Add new Swing sample based on circular dependencies usage
    git-svn-id: https://dependency-shot.googlecode.com/svn/trunk@257 2312e98e-8c1d-11de-b664-3b115b7b7a9b

---
+++ dependency-shot-samples/swing2/src/main/java/cx/ath/mancel01/dependencyshot/samples/vdm/gui/impl/VdmController.java
@@ -0,0 +1,89 @@
+/*
+ *  Copyright 2009-2010 Mathieu ANCELIN.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package cx.ath.mancel01.dependencyshot.samples.vdm.gui.impl;
+
+import cx.ath.mancel01.dependencyshot.api.annotations.InjectLogger;
+import cx.ath.mancel01.dependencyshot.samples.vdm.gui.Controller;
+import cx.ath.mancel01.dependencyshot.samples.vdm.gui.View;
+import cx.ath.mancel01.dependencyshot.samples.vdm.model.Vdm;
+import cx.ath.mancel01.dependencyshot.samples.vdm.service.RandomService;
+import java.util.Observable;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * The controller implementation of the app.
+ *
+ * @author Mathieu ANCELIN
+ */
+@Singleton
+public class VdmController extends Observable implements Controller {
+
+    /**
+     * Model of the app.
+     */
+    private Vdm model;
+
+    /**
+     * The random VDM service.
+     */
+    @Inject
+    private RandomService service;
+
+    /**
+     * The logger of the controller;
+     */
+    @Inject @InjectLogger
+    private Logger logger;
+
+    /**
+     * The view attached to this controller
+     */
+    @Inject
+    private View view;
+
+    /**
+     * Start the app.
+     */
+    public final void startApplication() {
+        this.addObserver(view);
+        updateModel();
+        view.start();
+        logger.info("Application now started ...");
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final Vdm getModel() {
+        return model;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final void updateModel() {
+        this.model = this.service.getRandomVdm();
+        logger.info("Model updated");
+        this.setChanged();
+        this.notifyObservers();  
+    }
+}
