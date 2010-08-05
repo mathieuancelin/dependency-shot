ab2f2023be9bdd983640becfd45eb32f23dc1f7f
mathieuancelin <mathieu.ancelin@gmail.com>
2010-08-05 12:24:53 +0000

    Added an utility class to easily build binding. Should be helpful for third-party ConfigurationHandler modules.

---
+++ dependency-shot-core/src/main/java/cx/ath/mancel01/dependencyshot/graph/builder/QualifiedBuilder.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2009-2010 Mathieu ANCELIN.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package cx.ath.mancel01.dependencyshot.graph.builder;
+
+import javax.inject.Provider;
+
+/**
+ * Interface for the binder.
+ * 
+ * @author Mathieu ANCELIN
+ */
+public interface QualifiedBuilder {
+    /**
+     * The target class for a binding.
+     *
+     * @param <T> type.
+     * @param to the targeted class for the binding.
+     * @return the actual binder.
+     */
+    <T> StagingBuilder to(Class<? extends T> to);
+    /**
+     * Specify a specific instance to bind with.
+     *
+     * @param <T> type.
+     * @param instance specify the instance.
+     * @return the actual binder.
+     */
+    <T> StagingBuilder toInstance(Object instance);
+    /**
+     * Specify a provider for the actual binding.
+     *
+     * @param <T> type.
+     * @param provider the provider for the binding.
+     * @return the actual binder.
+     */
+    <T> StagingBuilder providedBy(Provider<T> provider);
+}
