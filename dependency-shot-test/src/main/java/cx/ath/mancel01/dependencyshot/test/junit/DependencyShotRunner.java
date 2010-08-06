81f6bdcbf6f04c061a7b80194d6ceeff79576a65
mathieuancelin <mathieu.ancelin@gmail.com>
2010-08-06 12:25:36 +0000

    Finalize first version of the JUnit dependency-shot runner. Nice to have DI in JUnit tests :)

--- dependency-shot-test/src/main/java/cx/ath/mancel01/dependencyshot/test/junit/DependencyShotRunner.java
+++ dependency-shot-test/src/main/java/cx/ath/mancel01/dependencyshot/test/junit/DependencyShotRunner.java
@@ -17,6 +17,14 @@
 
 package cx.ath.mancel01.dependencyshot.test.junit;
 
+import cx.ath.mancel01.dependencyshot.DependencyShot;
+import cx.ath.mancel01.dependencyshot.api.DSInjector;
+import cx.ath.mancel01.dependencyshot.api.Stage;
+import cx.ath.mancel01.dependencyshot.graph.Binder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
 
@@ -26,9 +34,44 @@ import org.junit.runners.model.InitializationError;
  */
 public class DependencyShotRunner extends BlockJUnit4ClassRunner {
 
+    private static final Logger logger = Logger.getLogger(DependencyShotRunner.class.getSimpleName());
+
+    private Class<?> clazz;
+
+    private DSInjector injector;
+
     public DependencyShotRunner(Class<?> clazz) throws InitializationError {     
         super(clazz);
-        System.out.println("Test de la classe : " + clazz.getName());
+        this.clazz = clazz;
+        ConfigureWith config = clazz.getAnnotation(ConfigureWith.class);
+        if (config != null) {
+            Collection<Binder> binders = new ArrayList<Binder>();
+            for (Class cl : config.value()) {
+                try {
+                    binders.add((Binder) cl.newInstance());
+                } catch (Exception ex) {
+                    logger.log(Level.SEVERE, null, ex);
+                }
+            }
+            Binder[] bins = new Binder[binders.size()];
+            if (config.staging())
+                injector = DependencyShot.getInjector(Stage.TEST, binders.toArray(bins));
+            else
+                injector = DependencyShot.getInjector(binders.toArray(bins));
+        } else {
+            injector = DependencyShot.getInjector();
+        }
+        if (config.allowCircularDependencies())
+            injector.allowCircularDependencies(true);
+        logger.info(new StringBuilder()
+                .append("Running test class \"")
+                .append(clazz.getSimpleName())
+                .append(".java\" with Dependency-Shot container ...")
+                .toString());
     }
-    
+
+    @Override
+	protected Object createTest() throws Exception {
+        return injector.getInstance(getTestClass().getJavaClass());
+	}
 }
