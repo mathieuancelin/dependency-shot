f8dbe6351d2b3403ad2c626ca2dbce7349974695
mathieuancelin <mathieu.ancelin@gmail.com>
2010-08-29 19:57:36 +0000

    Add unit tests for @Nullable

---
+++ dependency-shot-core/src/test/java/cx/ath/mancel01/dependencyshot/test/nullable/NotNullableClient.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright 2010 mathieuancelin.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package cx.ath.mancel01.dependencyshot.test.nullable;
+
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Mathieu ANCELIN
+ */
+public class NotNullableClient {
+
+    private Service service1;
+
+    private Service service2;
+
+    @Inject
+    private Service service3;
+
+    @Inject
+    public NotNullableClient(Service service1) {
+        this.service1 = service1;
+    }
+
+    @Inject
+    public void setService2(Service service2) {
+        this.service2 = service2;
+    }
+
+    public Service getService1() {
+        return service1;
+    }
+
+    public Service getService2() {
+        return service2;
+    }
+
+    public Service getService3() {
+        return service3;
+    }
+
+}
---
+++ dependency-shot-core/src/test/java/cx/ath/mancel01/dependencyshot/test/nullable/NullableClient.java
@@ -0,0 +1,58 @@
+/*
+ *  Copyright 2010 mathieuancelin.
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+
+package cx.ath.mancel01.dependencyshot.test.nullable;
+
+import cx.ath.mancel01.dependencyshot.api.Nullable;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author Mathieu ANCELIN
+ */
+public class NullableClient {
+
+    private Service service1;
+
+    private Service service2;
+
+    @Inject @Nullable
+    private Service service3;
+
+    @Inject
+    public NullableClient(@Nullable Service service1) {
+        this.service1 = service1;
+    }
+
+    @Inject
+    public void setService2(@Nullable Service service2) {
+        this.service2 = service2;
+    }
+
+    public Service getService1() {
+        return service1;
+    }
+
+    public Service getService2() {
+        return service2;
+    }
+
+    public Service getService3() {
+        return service3;
+    }
+
+}
